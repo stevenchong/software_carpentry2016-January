38494d65a97095022e28cd9a5328a65cc4a51c0d
stevenchong <stevenchong@email.arizona.edu>
2016-01-30 16:23:02 -0700

    initial commit

---
+++ conditional.py
@@ -0,0 +1,6 @@
+x = 7
+
+if x < 10:
+    print("that makes sense")
+else:
+    print("I don't think this will happen")
---
+++ iteration.py
@@ -0,0 +1,8 @@
+books = ["Tales of Two Cities", "Harry Potter", "Lord of the Rings"]
+nums = range(100)
+
+#for book in books:
+#    print (book)
+    
+for num in nums:
+    print (num)
